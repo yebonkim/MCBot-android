0be31663a5b1fe6826bd46f2f03ce24f22ed1001
lipnus <sunpil13@korea.ac.kr>
2018-08-11 21:40:23 +0900

    update bot fragment

--- app/src/main/java/com/example/mcbot/fragment/BotFragment.java
+++ app/src/main/java/com/example/mcbot/fragment/BotFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.example.mcbot.R;
@@ -27,8 +28,8 @@ import butterknife.ButterKnife;
 public class BotFragment extends Fragment {
     Context context;
 
-    @BindView(R.id.bot_btn)
-    Button bot_btn;
+//    @BindView(R.id.bot_btn)
+//    Button bot_btn;
 
     @BindView(R.id.bot_1_iv)
     ImageView bot1_Iv;
@@ -38,6 +39,26 @@ public class BotFragment extends Fragment {
 
     @BindView(R.id.bot_3_iv)
     ImageView bot3_Iv;
+
+    @BindView(R.id.bot_check1_iv)
+    ImageView check1_iv;
+
+    @BindView(R.id.bot_check2_iv)
+    ImageView check2_iv;
+
+    @BindView(R.id.bot_check3_iv)
+    ImageView check3_iv;
+
+    @BindView(R.id.bot1_ll)
+    LinearLayout bot1LL;
+
+    @BindView(R.id.bot2_ll)
+    LinearLayout bot2LL;
+
+    @BindView(R.id.bot3_ll)
+    LinearLayout bot3LL;
+
+    Boolean[] chk;
 
     public BotFragment() {
 
@@ -64,14 +85,70 @@ public class BotFragment extends Fragment {
 
         context = getContext();
 
-        bot_btn.setOnClickListener(new Button.OnClickListener() {
+
+        chk = new Boolean[3];
+        chk[0] = true;
+        chk[1] = true;
+        chk[2] = true;
+
+        imageInit();
+        checkClick(context);
+    }
+
+    void checkClick(final Context context){
+        bot1LL.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent iT = new Intent(context, BotSettingActivity.class);
-                startActivity(iT);
+              if( chk[0] ){
+                  Glide.with(context)
+                          .load(R.drawable.check_off)
+                          .into(check1_iv);
+                  chk[0] = false;
+              }else{
+                  Glide.with(context)
+                          .load(R.drawable.check_on)
+                          .into(check1_iv);
+                  chk[0] = true;
+              }
             }
         });
 
+        bot2LL.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( chk[1] ){
+                    Glide.with(context)
+                            .load(R.drawable.check_off)
+                            .into(check2_iv);
+                    chk[1] = false;
+                }else{
+                    Glide.with(context)
+                            .load(R.drawable.check_on)
+                            .into(check2_iv);
+                    chk[1] = true;
+                }
+            }
+        });
+
+        bot3LL.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( chk[2] ){
+                    Glide.with(context)
+                            .load(R.drawable.check_off)
+                            .into(check3_iv);
+                    chk[2] = false;
+                }else{
+                    Glide.with(context)
+                            .load(R.drawable.check_on)
+                            .into(check3_iv);
+                    chk[2] = true;
+                }
+            }
+        });
+    }
+
+    void imageInit(){
         Glide.with(this)
                 .load(R.drawable.jaeseok)
                 .into(bot1_Iv);
@@ -83,6 +160,18 @@ public class BotFragment extends Fragment {
         Glide.with(this)
                 .load(R.drawable.dongseok)
                 .into(bot3_Iv);
+
+        Glide.with(this)
+                .load(R.drawable.check_on)
+                .into(check1_iv);
+
+        Glide.with(this)
+                .load(R.drawable.check_on)
+                .into(check2_iv);
+
+        Glide.with(this)
+                .load(R.drawable.check_on)
+                .into(check3_iv);
     }
 
 }
