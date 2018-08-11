83841c483ab5fcf2cbbf5f79c30ad262577da882
coldmee <coldmee@naver.com>
2018-08-12 02:18:52 +0900

    merge

---
+++ app/src/main/java/com/example/mcbot/activity/NaverApiMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.mcbot.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NaverApiMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_naver_api_map);
+    }
+}
